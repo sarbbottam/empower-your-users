349b9f01d4ece9a57993e8be14edbc7c69cce7f9
Sarbbottam Bandyopadhyay <sarbbottam@gmail.com>
2016-05-21 17:20:08 -0700

    chore(build:html): pages.js is not required

--- scripts/build.js
+++ scripts/build.js
@@ -2,16 +2,17 @@ import fs from 'fs';
 import path from 'path';
 import {html as beautify} from 'js-beautify';
 
-import pages from './pages';
-const dir = 'dist';
+const DIST_DIR = 'dist';
+const PAGES_DIR = '../src/pages';
 
-if (!fs.existsSync(dir)) {
-  fs.mkdirSync(dir);
+if (!fs.existsSync(DIST_DIR)) {
+  fs.mkdirSync(DIST_DIR);
 }
 
-Object.keys(pages).forEach(page => {
-  const markup = beautify(pages[page](), {indent_size: 2}); // eslint-disable-line camelcase
-  const fileName = path.join(process.cwd(), 'dist', `${page}.html`);
+fs.readdirSync(path.resolve(__dirname, PAGES_DIR)).forEach(page => {
+  let markup = require(path.resolve(__dirname, PAGES_DIR, page)).default();
+  markup = beautify(markup, {indent_size: 2}); // eslint-disable-line camelcase
+  const fileName = path.join(process.cwd(), 'dist', `${page.replace('js', 'html')}`);
 
   fs.writeFile(fileName, markup, error => {
     if (error) {
--- scripts/pages.js
+++
@@ -1,7 +0,0 @@
-import formAccessible from '../src/pages/form-accessible';
-import formInaccessible from '../src/pages/form-inaccessible';
-
-export default {
-  'form-accessible': formAccessible,
-  'form-inaccessible': formInaccessible
-};
