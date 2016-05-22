e052400667e81322048ec07d9eb578b330314141
Sarbbottam Bandyopadhyay <sarbbottam@gmail.com>
2016-05-22 15:12:24 -0700

    refactor(form): full-name
    instead of first & last name

--- src/components/form-accessible.jsx
+++ src/components/form-accessible.jsx
@@ -7,20 +7,26 @@ export default class FormAccessible extends React.Component {
   render() {
     return (
       <form noValidate>
-        <fieldset>
-          <legend className="clipped">Name</legend>
-          <div className="row row-input">
-            <Input name="firstName" wrapperClassName="col col-1-2 p-e" id="first-name" placeholder="First Name" required/>
-            <Input name="lastName" wrapperClassName="col col-1-2 p-s" id="last-name" placeholder="Last Name" required/>
-          </div>
-        </fieldset>
+        <div className="row row-input">
+          <Input name="fullName" wrapperClassName="col col-1-1" id="full-name" placeholder="Full Name" required/>
+        </div>
         <fieldset>
           <legend className="clipped">Address</legend>
-          <Input name="addressLine1" wrapperClassName="row-input" id="address-line-1" placeholder="Address line 1" required/>
-          <Input name="addressLine2" wrapperClassName="row-input" id="address-line-2" placeholder="Address line 2"/>
-          <Input name="city" wrapperClassName="row-input" id="city" placeholder="City" className="w-a" required/>
-          <Select name="state" wrapperClassName="row-input" id="state" label="State" className="w-a" required options={states}/>
-          <Input name="postalCode" wrapperClassName="row-input" id="postal-code" placeholder="Postal code" className="w-a" required/>
+          <div className="row row-input">
+            <Input name="addressLine1" wrapperClassName="col col-1-1" id="address-line-1" placeholder="Address line 1" required/>
+          </div>
+          <div className="row row-input">
+            <Input name="addressLine2" wrapperClassName="col col-1-1" id="address-line-2" placeholder="Address line 2"/>
+          </div>
+          <div className="row row-input">
+            <Input name="city" wrapperClassName="col" id="city" placeholder="City" className="w-a" required/>
+          </div>
+          <div className="row row-input">
+            <Select name="state" wrapperClassName="col" id="state" label="State" className="w-a" required options={states}/>
+          </div>
+          <div className="row row-input">
+            <Input name="postalCode" wrapperClassName="col" id="postal-code" placeholder="Postal code" className="w-a" required/>
+          </div>
         </fieldset>
         <input name="tos" type="checkbox" id="tos" required/> <label htmlFor="tos">I agree to the terms and condition </label>
         <div className="row-button">
--- src/components/form-inaccessible.jsx
+++ src/components/form-inaccessible.jsx
@@ -8,14 +8,26 @@ export default class FormAccessible extends React.Component {
     return (
       <form noValidate>
         <div className="row row-input">
-          <Input screwA11Y name="firstName" wrapperClassName="col col-1-2 p-e" id="first-name" placeholder="First Name" required/>
-          <Input screwA11Y name="lastName" wrapperClassName="col col-1-2 p-s" id="last-name" placeholder="Last Name" required/>
+          <Input screwA11Y name="fullName" wrapperClassName="col col-1-1" id="full-name" placeholder="Full Name" required/>
         </div>
-        <Input screwA11Y name="addressLine1" wrapperClassName="row-input" id="address-line-1" placeholder="Address line 1" required/>
-        <Input screwA11Y name="addressLine2" wrapperClassName="row-input" id="address-line-2" placeholder="Address line 2"/>
-        <Input screwA11Y name="city" wrapperClassName="row-input" id="city" placeholder="City" className="w-a" required/>
-        <Select screwA11Y name="state" wrapperClassName="row-input" id="state" label="State" className="w-a" required options={states}/>
-        <Input screwA11Y name="postalCode" wrapperClassName="row-input" id="postal-code" placeholder="Postal code" className="w-a" required/>
+        <fieldset>
+          <legend className="clipped">Address</legend>
+          <div className="row row-input">
+            <Input screwA11Y name="addressLine1" wrapperClassName="col col-1-1" id="address-line-1" placeholder="Address line 1" required/>
+          </div>
+          <div className="row row-input">
+            <Input screwA11Y name="addressLine2" wrapperClassName="col col-1-1" id="address-line-2" placeholder="Address line 2"/>
+          </div>
+          <div className="row row-input">
+            <Input screwA11Y name="city" wrapperClassName="col" id="city" placeholder="City" className="w-a" required/>
+          </div>
+          <div className="row row-input">
+            <Select screwA11Y name="state" wrapperClassName="col" id="state" label="State" className="w-a" required options={states}/>
+          </div>
+          <div className="row row-input">
+            <Input screwA11Y name="postalCode" wrapperClassName="col" id="postal-code" placeholder="Postal code" className="w-a" required/>
+          </div>
+        </fieldset>
         <input name="tos" type="checkbox" id="tos" required/> I agree to the terms and condition
         <div className="row-button">
           <button type="submit" className="button button-primary">Submit</button>
