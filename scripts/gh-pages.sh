6096394d888f225492241328d04f1c525a2e6191
Sarbbottam Bandyopadhyay <sarbbottam@gmail.com>
2016-06-08 15:48:25 -0700

    chore(gh-pages): deploy to gh-pages

---
+++ scripts/gh-pages.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+rm -rf dist || exit 0;
+if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+  npm run build;
+  ( cd dist
+    git init
+    git config user.name "sarbbottam"
+    git config user.email "sarbbottam@gmail.com"
+    git add .
+    git commit -m "initial commit"
+    git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+  )
+else
+   echo "not publising to gh-pages"
+fi
