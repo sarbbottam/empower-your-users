c17bd967b89ff022452784e62c2ad2a4d24509fc
Sarbbottam Bandyopadhyay <sarbbottam@gmail.com>
2016-06-12 13:56:17 -0700

    docs(button): added annotation

--- src/components/button-accessible.jsx
+++ src/components/button-accessible.jsx
@@ -4,7 +4,12 @@ export default class Button extends React.Component {
   render() {
     return (
       <div className="row-input">
-        <button className="button button-secondary w-100">Surprise me!</button>
+        <ul>
+          <li>✅'Surprise me! - button' is reachable via keyboard</li>
+          <li>✅'Surprise me! - button' can be activated by pressing `enter` and `space`, similar to clicking</li>
+          <li>✅'Surprise me! - button' is treated as `button` by assistive technologies, like screen reading software.</li>
+        </ul>
+        <button className="button button-secondary w-100">Surprise me! - button</button>
       </div>
     );
   }
--- src/components/button-inaccessible.jsx
+++ src/components/button-inaccessible.jsx
@@ -3,20 +3,40 @@ import React from 'react';
 export default class Button extends React.Component {
   render() {
     return (
-      <div>
-        <div className="row-input">
+      <ul>
+        <li className="row-input">
+          <ul>
+            <li>❌'Surprise me! - span' is not reachable via keyboard</li>
+            <li>❌'Surprise me! - span' can not be activated by pressing `enter` and `space`, but only by clicking</li>
+            <li>❌'Surprise me! - span' is not treated as `button`, but as text by assistive technologies, like screen reading software.</li>
+          </ul>
           <span className="button button-secondary w-100">Surprise me! - span</span>
-        </div>
-        <div className="row-input">
+        </li>
+        <li className="row-input">
+          <ul>
+            <li>✅'Surprise me! - span/tabindex' is reachable via keyboard</li>
+            <li>❌'Surprise me! - span/tabindex' can not be activated by pressing `enter` and `space`, but only by clicking</li>
+            <li>❌'Surprise me! - span/tabindex' is not treated as `button`, but as text by assistive technologies, like screen reading software</li>
+          </ul>
           <span tabIndex="0" className="button button-secondary w-100">Surprise me! - span/tabindex</span>
-        </div>
-        <div className="row-input">
+        </li>
+        <li className="row-input">
+          <ul>
+            <li>✅'Surprise me! a/href' is reachable via keyboard</li>
+            <li>❌'Surprise me! a/href' can not be activated by pressing `space`, but only by clicking and pressing `enter`</li>
+            <li>❌'Surprise me! a/href' is not treated as `button`, but as link by assistive technologies, like screen reading software</li>
+          </ul>
           <a href="#" className="button button-secondary w-100">Surprise me! a/href</a>
-        </div>
-        <div className="row-input">
+        </li>
+        <li className="row-input">
+          <ul>
+            <li>✅'Surprise me! a/href/role' is reachable via keyboard</li>
+            <li>❌'Surprise me! a/href/role' can not be activated by pressing `space`, but only by clicking and pressing `enter`</li>
+            <li>✅'Surprise me! a/href/role' is not treated as button by assistive technologies, like screen reading software</li>
+          </ul>
           <a href="#" role="button" className="button button-secondary w-100">Surprise me! a/href/role</a>
-        </div>
-      </div>
+        </li>
+      </ul>
     );
   }
 }
